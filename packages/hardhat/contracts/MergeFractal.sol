11deff5e3c98e69ea4df1d604980b313ad577b5c
David Ryan <dr-github@outlook.com>
2022-08-15 22:42:14 +0700

    Make internal

--- packages/hardhat/contracts/MergeFractal.sol
+++ packages/hardhat/contracts/MergeFractal.sol
@@ -60,9 +60,9 @@ contract MergeFractal is ERC721, Ownable {
     // RELEASE THE MERGE FRACTALS!
   }
 
-  mapping (uint256 => uint256) public generator;
-  mapping (uint256 => address) public mintooor;
-  uint256 mintDeadline = block.timestamp + 24 hours;
+  mapping (uint256 => uint256) internal generator;
+  mapping (uint256 => address) internal mintooor;
+  uint256 mintDeadline = block.timestamp + 60 days;
 
   function mintItem()
       public
@@ -77,30 +77,12 @@ contract MergeFractal is ERC721, Ownable {
       return id;
   }
 
-  // TODO: remove this, testing only
-  function getGenTEMP(uint256 id) public view returns (uint256) {
-    return generator[id];
-  }
-
-  // TODO: make internal
   // Get up to 8 pseudorandom bits from the 256-bit pseudorandom generator
-  function getUint8(uint256 id, uint8 startBit, uint8 bits) public view returns (uint8) {
+  function getUint8(uint256 id, uint8 startBit, uint8 bits) internal view returns (uint8) {
     return uint8(generator[id] >> startBit) & masks8[bits - 1];
   }
 
-  // TODO: make internal
-  function getDur(uint256 id, uint8 arraySection) public view returns (string memory) {
-    uint8 startBitDur = sectionColStartBits[arraySection] + 3;
-    uint8 idx = 8 * arraySection + getUint8(id, startBitDur, 3); // 3 bits = 8 duration choices
-    return string(abi.encodePacked(
-      ' dur="',
-      ToColor.uint2str(durations[idx]),
-      's"'
-    ));
-  }
-
-  // TODO: make internal
-  function getRGBA(uint256 id, uint8 arraySection, string memory alpha) public view returns (string memory) {
+  function getRGBA(uint256 id, uint8 arraySection, string memory alpha) internal view returns (string memory) {
     uint8 startBit = sectionColStartBits[arraySection];
     // Array section values are 0, 1, 2 or 3 (0 is darkest, 3 is lightest)
     // These sections give colours 0-7, 8-15, 16-23, 24-31
@@ -131,9 +113,11 @@ contract MergeFractal is ERC721, Ownable {
   function getAllAttributes(uint256 id) public view returns (string memory) {
     return string(abi.encodePacked(
       '[',
-      getTrait("Core Dev", getCoreDevName(id)),
+      getTrait("Dev", getCoreDevName(id)),
       ',',
       getTrait("Team", getTeamName(id)),
+      ',',
+      getTrait("Saying", getSaying(id)),
       ']'
     ));
   }
@@ -173,7 +157,7 @@ contract MergeFractal is ERC721, Ownable {
     return svg;
   }
 
-  function renderDisk(uint256 id) public view returns (string memory) {
+  function renderDisk(uint256 id) internal view returns (string memory) {
     string memory render = '';
     render = string(abi.encodePacked(
       // render,
@@ -184,7 +168,7 @@ contract MergeFractal is ERC721, Ownable {
     return render;    
   }
 
-  function getLinesTransform(uint8 arraySection) public view returns (string memory) {
+  function getLinesTransform(uint8 arraySection) internal view returns (string memory) {
     uint8 num1 = sectionLineTranslates[arraySection];
     uint16 num2 = 200 - num1;
     string memory render = '';
@@ -201,8 +185,18 @@ contract MergeFractal is ERC721, Ownable {
     return render;    
   }
 
+  function getDur(uint256 id, uint8 arraySection) internal view returns (string memory) {
+    uint8 startBitDur = sectionColStartBits[arraySection] + 3;
+    uint8 idx = 8 * arraySection + getUint8(id, startBitDur, 3); // 3 bits = 8 duration choices
+    return string(abi.encodePacked(
+      ' dur="',
+      ToColor.uint2str(durations[idx]),
+      's"'
+    ));
+  }
+
   // Uses 6 random bits
-  function renderLines(uint256 id, uint8 arraySection, string memory maxAngleText) public view returns (string memory) {
+  function renderLines(uint256 id, uint8 arraySection, string memory maxAngleText) internal view returns (string memory) {
     string memory rgba = getRGBA(id, arraySection, "0.90");
     string memory linesPath = ' d="M 11 1145 L 11 855 M 32 1251 L 32 749 M 53 1322 L 53 678 M 74 1379 L 74 621 M 96 1427 L 96 573 M 117 1469 L 117 531 M 138 1507 L 138 493 M 160 1542 L 160 458 M 181 1574 L 181 426 M 202 1603 L 202 397 M 223 1630 L 223 370 M 245 1655 L 245 345 M 266 1679 L 266 321 M 287 1701 L 287 299 M 309 1722 L 309 278 M 330 1742 L 330 258 M 351 1761 L 351 239 M 372 1778 L 372 222 M 394 1795 L 394 205 M 415 1811 L 415 189 M 436 1826 L 436 174 M 457 1840 L 457 160 M 479 1853 L 479 147 M 500 1866 L 500 134 M 521 1878 L 521 122 M 543 1889 L 543 111 M 564 1900 L 564 100 M 585 1910 L 585 90 M 606 1919 L 606 81 M 628 1928 L 628 72 M 649 1936 L 649 64 M 670 1944 L 670 56 M 691 1951 L 691 49 M 713 1958 L 713 42 M 734 1964 L 734 36 M 755 1970 L 755 30 M 777 1975 L 777 25 M 798 1979 L 798 21 M 819 1984 L 819 16 M 840 1987 L 840 13 M 862 1990 L 862 10 M 883 1993 L 883 7 M 904 1995 L 904 5 M 926 1997 L 926 3 M 947 1999 L 947 1 M 968 1999 L 968 1 M 989 2000 L 989 0 M 1011 2000 L 1011 0 M 1032 1999 L 1032 1 M 1053 1999 L 1053 1 M 1074 1997 L 1074 3 M 1096 1995 L 1096 5 M 1117 1993 L 1117 7 M 1138 1990 L 1138 10 M 1160 1987 L 1160 13 M 1181 1984 L 1181 16 M 1202 1979 L 1202 21 M 1223 1975 L 1223 25 M 1245 1970 L 1245 30 M 1266 1964 L 1266 36 M 1287 1958 L 1287 42 M 1309 1951 L 1309 49 M 1330 1944 L 1330 56 M 1351 1936 L 1351 64 M 1372 1928 L 1372 72 M 1394 1919 L 1394 81 M 1415 1910 L 1415 90 M 1436 1900 L 1436 100 M 1457 1889 L 1457 111 M 1479 1878 L 1479 122 M 1500 1866 L 1500 134 M 1521 1853 L 1521 147 M 1543 1840 L 1543 160 M 1564 1826 L 1564 174 M 1585 1811 L 1585 189 M 1606 1795 L 1606 205 M 1628 1778 L 1628 222 M 1649 1761 L 1649 239 M 1670 1742 L 1670 258 M 1691 1722 L 1691 278 M 1713 1701 L 1713 299 M 1734 1679 L 1734 321 M 1755 1655 L 1755 345 M 1777 1630 L 1777 370 M 1798 1603 L 1798 397 M 1819 1574 L 1819 426 M 1840 1542 L 1840 458 M 1862 1507 L 1862 493 M 1883 1469 L 1883 531 M 1904 1427 L 1904 573 M 1926 1379 L 1926 621 M 1947 1322 L 1947 678 M 1968 1251 L 1968 749 M 1989 1145 L 1989 855 "';
     string memory render = '';
@@ -239,7 +233,7 @@ contract MergeFractal is ERC721, Ownable {
     return render;     
   }
 
-  function renderDiskAndLines(uint256 id) public view returns (string memory) {
+  function renderDiskAndLines(uint256 id) internal view returns (string memory) {
     string memory render = '';    
     render = string(abi.encodePacked(
       // render,
@@ -252,7 +246,7 @@ contract MergeFractal is ERC721, Ownable {
     return render;    
   }
 
-  function renderBorder(uint256 id) public view returns (string memory) {
+  function renderBorder(uint256 id) internal view returns (string memory) {
     string memory rgba0 = getRGBA(id, 0, "0.9");
     string memory rgba3 = getRGBA(id, 3, "0.8");
     string memory render = '';    
@@ -279,11 +273,11 @@ contract MergeFractal is ERC721, Ownable {
     return coreDevTeamIndices[getCoreDevIdx(id)];
   }
 
-  function getCoreDevName(uint256 id) public view returns (string memory) {
+  function getCoreDevName(uint256 id) internal view returns (string memory) {
     return coreDevNames[getCoreDevIdx(id)];
   }
 
-  function getTeamName(uint256 id) public view returns (string memory) {
+  function getTeamName(uint256 id) internal view returns (string memory) {
     return teams[getTeamIdx(id)];
   }
 
@@ -306,11 +300,11 @@ contract MergeFractal is ERC721, Ownable {
     return render;       
   }
 
-  function getSaying(uint256 id) public view returns (string memory) {
+  function getSaying(uint256 id) internal view returns (string memory) {
     return sayings[getUint8(id, SAYING_START_BIT, 8) % SAYING_ARRAY_LEN];
   }
 
-  function renderText(uint256 id) public view returns (string memory) {
+  function renderText(uint256 id) internal view returns (string memory) {
     string memory rgba0 = IS_TESTNET ? 'rgba(96,96,96,0.9)' : getRGBA(id, 0, "1");
     string memory render = '';    
     render = string(abi.encodePacked(
@@ -333,7 +327,7 @@ contract MergeFractal is ERC721, Ownable {
     return render;    
   }
 
-  function renderEthereum(uint256 id) public view returns (string memory) {
+  function renderEthereum(uint256 id) internal view returns (string memory) {
     string memory rgbaLine = getRGBA(id, 1, "0.85");
     string memory rgbaFill = getRGBA(id, 2, "0.60");
     string memory render = '';    
