36e645ca22ca69e178e655de24815973d42e9c50
David Ryan <dr-github@outlook.com>
2022-08-19 18:01:13 +0700

    Simplify to save gas

--- packages/hardhat/contracts/FractalStrings.sol
+++ packages/hardhat/contracts/FractalStrings.sol
@@ -5,50 +5,30 @@ import './SharedFnsAndData.sol';
 
 contract FractalStrings {
 
-  // Paths are (approx) in a box [-50, -50] to [50, 50] so require scale 1/100 to fix in a unit box
-  uint8 internal constant PATHS_LEN = 6;
-  string[PATHS_LEN] internal pathData = [
-    'M -50 -50 L -50 50 50 50 50 -50 -50 -50',
-    'M -50 -50 L -50 50 50 50 50 0 0 -50 -50 -50',
-    'M -50 -50 L -50 50 50 50 50 0 0 0 0 -50 -50 -50',
-    'M -50 -50 L -50 50 50 50 0 0 50 -50 -50 -50',
-    'M -50 -50 L -50 50 0 33 50 50 50 -50 0 -33 -50 -50',
-    'M -50 -50 L -33 0 -50 50 0 33 50 50 33 0 50 -50 0 -33 -50 -50'
-  ];
-
   SharedFnsAndData sfad;
   constructor(address sfadAddress) public {
     // 2nd contract because 1st contract ran out of code space...
     sfad = SharedFnsAndData(sfadAddress);
   }
 
-  function defineShape(uint256 gen, uint8 shapeIdx, uint8 colourIdxFill, uint8 colourIdxLine) internal view returns (string memory) {
+  // To tesselate the Ethereum diamond, shapes are rectangles
+  function defineShape(uint256 gen, uint8 sideIdx, uint8 colourIdxFill) internal view returns (string memory) {
     return string(abi.encodePacked(
-      '<path id="shape',
-      sfad.uint2str(shapeIdx),
-      '" d="',
-      pathData[0],
-      // pathData[sfad.getUint8(gen, sfad.getSectionShapesStartBits(shapeIdx), 4) % PATHS_LEN],
-      '" fill="',
-      sfad.getRGBA(gen, colourIdxFill, "0.75"),
+      '<rect id="shape',
+      sfad.uint2str(sideIdx),
+      '" x="-0.5" y="-0.5" width="1" height="1" rx="0.25" fill="',
+      sfad.getRGBA(gen, colourIdxFill, "0.70"),
       '" stroke="',
-      sfad.getRGBA(gen, 0, "0.85"),
-      // sfad.getRGBA(gen, colourIdxLine, "0.80"),
-      '" stroke-width="3px" transform="scale(0.01 -0.01)" />'
+      sfad.getRGBA(gen, 0, "0.80"),
+      '" stroke-width="0.15px"/>'
     ));
   }
 
   // Defines shape0, shape1, ... shape7
   function defineAllShapes(uint256 gen) internal view returns (string memory) {
     return string(abi.encodePacked(
-      defineShape(gen, 0, 1, 0),
-      defineShape(gen, 1, 1, 1),
-      defineShape(gen, 2, 1, 2),
-      defineShape(gen, 3, 1, 3),
-      defineShape(gen, 4, 2, 0),
-      defineShape(gen, 5, 2, 1),
-      defineShape(gen, 6, 2, 2),
-      defineShape(gen, 7, 2, 3)
+      defineShape(gen, 0, 1),
+      defineShape(gen, 1, 2)
     ));
   }
 
@@ -57,19 +37,15 @@ contract FractalStrings {
   function getIteration1Item(uint8 sideIdx, uint8 itemIdx) private view returns (string memory) {
     uint8 idx = 4 * sideIdx + itemIdx;
     return string(abi.encodePacked(
-      '<g transform="translate(-0.5, 0)">',
-      '<animateTransform attributeName="transform" attributeType="XML" type="translate"',
+      '<g transform="translate(-0.5, 0)"><animateTransform attributeName="transform" attributeType="XML" type="translate"',
       sfad.calcValuesFull(xStarts[idx], xEnds[idx], '0.', itemIdx > 1 ? " -0.25" : " 0.25"),
       ANIM_DUR,
-      ' repeatCount="indefinite" additive="sum"/>',
-      '<animateTransform attributeName="transform" attributeType="XML" type="scale"',
+      ' repeatCount="indefinite" additive="sum"/><animateTransform attributeName="transform" attributeType="XML" type="scale"',
       sfad.calcValuesFull(500, 250, '0.', ' 0.5'),
       ANIM_DUR,
-      ' repeatCount="indefinite" additive="sum"/>',
-      '<use href="#shape',
-      sfad.uint2str(idx),
-      '"/>',
-      '</g>'
+      ' repeatCount="indefinite" additive="sum"/><use href="#shape',
+      sfad.uint2str(sideIdx),
+      '"/></g>'
     ));
   }
 
@@ -139,8 +115,8 @@ contract FractalStrings {
     ));
   }
 
-  uint8 internal constant RENDER_ITERATION = 3;
-  string internal constant ANIM_DUR = ' dur="10s"';
+  uint8 internal constant RENDER_ITERATION = 4;
+  string internal constant ANIM_DUR = ' dur="5s"';
 
   function renderEthereums(uint256 gen) public view returns (string memory) {
     return string(abi.encodePacked(
@@ -152,8 +128,8 @@ contract FractalStrings {
       defineIterationN(1, 2),
       defineIterationN(0, 3),
       defineIterationN(1, 3),
-      // defineIterationN(0, 4),
-      // defineIterationN(1, 4), // up to iteration 4 can be rendered
+      defineIterationN(0, 4),
+      defineIterationN(1, 4), // up to iteration 4 can be rendered
       '</defs>',
       renderEthereum(0, RENDER_ITERATION, 125),
       renderEthereum(1, RENDER_ITERATION, 275)
