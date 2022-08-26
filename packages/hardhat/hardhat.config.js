1dd8fc0643594912f5efc6276f8d3870a6cf8d59
David Ryan <dr-github@outlook.com>
2022-08-26 10:48:14 +0700

    Replace Etherscan

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -130,7 +130,7 @@ module.exports = {
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8"
+    apiKey: "GJ3A3PAWU9YG2RY8CU4JKDRAUVHSUPB22Y"
   }
 };
 
