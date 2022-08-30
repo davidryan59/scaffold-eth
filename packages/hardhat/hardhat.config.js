ade92569753097a878e2a9c72507c730afc5cb27
David Ryan <dr-github@outlook.com>
2022-08-30 23:11:05 +0700

    Remove hardhat compiler

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -109,15 +109,6 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
-      },
-      {
         version: "0.6.7",
         settings: {
           optimizer: {
