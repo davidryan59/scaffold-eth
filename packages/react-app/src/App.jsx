8281351a262024c93be9c2664dfdb7cd6fab4a52
David Ryan <dr-github@outlook.com>
2022-09-04 18:16:16 +0700

    Refactoring App

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -366,7 +366,7 @@ function App() {
     },
   ];
 
-  const renderFractalListItem = item => {
+  function FractalAttributes({ item }) {
     const id = item.id.toNumber();
     const attributesDataSource = item.attributes.map((obj, idx) => ({
       key: idx,
@@ -374,55 +374,67 @@ function App() {
       value: obj.value,
     }));
     return (
-      <List.Item key={"F" + id}>
-        <Card
-          title={
-            <div>
-              <span style={{ fontSize: 18, marginRight: 8 }}>{item.name}</span>
-            </div>
-          }
+      <div style={{width: "400px"}}>
+        <Table dataSource={attributesDataSource} columns={attributesColumns} size="small" pagination={false} /> 
+        <p></p>
+        owner:{" "}
+        <Address
+          address={item.owner}
+          ensProvider={mainnetProvider}
+          blockExplorer={blockExplorer}
+          fontSize={16}
+        />
+        <p></p>
+        <AddressInput
+          ensProvider={mainnetProvider}
+          placeholder="transfer to address"
+          value={transferToAddresses[id]}
+          onChange={newValue => {
+            const update = {};
+            update[id] = newValue;
+            setTransferToAddresses({ ...transferToAddresses, ...update });
+          }}
+        />
+        <Button
+          onClick={() => {
+            console.log("writeContracts", writeContracts);
+            tx(writeContracts.MergeFractal.transferFrom(address, transferToAddresses[id], id));
+          }}
         >
-          {/* <a href={"https://opensea.io/assets/"+(readContracts && readContracts.MergeFractal && readContracts.MergeFractal.address)+"/"+item.id} target="_blank"> */}
-          <a href={`/token/${item.id}`}>
-            <img src={item.image} style={{background: 'rgba(128,128,128,0.1)'}}/>
-          </a>
-        </Card>
+          Transfer
+        </Button>
+      </div>
+    );
+  }
 
-        <div style={{width: "400px"}}>
-          <Table dataSource={attributesDataSource} columns={attributesColumns} size="small" pagination={false} /> 
-          <p></p>
-          owner:{" "}
-          <Address
-            address={item.owner}
-            ensProvider={mainnetProvider}
-            blockExplorer={blockExplorer}
-            fontSize={16}
-          />
-          <p></p>
-          <AddressInput
-            ensProvider={mainnetProvider}
-            placeholder="transfer to address"
-            value={transferToAddresses[id]}
-            onChange={newValue => {
-              const update = {};
-              update[id] = newValue;
-              setTransferToAddresses({ ...transferToAddresses, ...update });
-            }}
-          />
-          <Button
-            onClick={() => {
-              console.log("writeContracts", writeContracts);
-              tx(writeContracts.MergeFractal.transferFrom(address, transferToAddresses[id], id));
-            }}
-          >
-            Transfer
-          </Button>
-        </div>
+  function FractalDisplay({ item }) {
+    return (
+      <Card
+        title={
+          <div>
+            <span style={{ fontSize: 18, marginRight: 8 }}>{item.name}</span>
+          </div>
+        }
+      >
+        {/* <a href={"https://opensea.io/assets/"+(readContracts && readContracts.MergeFractal && readContracts.MergeFractal.address)+"/"+item.id} target="_blank"> */}
+        <a href={`/token/${item.id}`}>
+          <img src={item.image} style={{background: 'rgba(128,128,128,0.1)'}}/>
+        </a>
+      </Card>
+    );
+  }
+
+  const renderFractalListItem = item => {
+    const id = item.id.toNumber();
+    return (
+      <List.Item key={"F" + id}>
+        <FractalDisplay item={item} />
+        <FractalAttributes item={item} />
       </List.Item>
     );
   };
 
-  function FractalList({maxCount, dataSource}) {
+  function FractalList({ maxCount, dataSource }) {
     const filteredDataSource = dataSource ? dataSource.slice(0, maxCount || dataSource.length) : [];
     return (
       <div style={{ width: "960px", margin: "auto", paddingBottom: 256 }}>
@@ -462,7 +474,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* ✏️ Edit the header and change the title to your project name */}
       <Header networkName={networkName} />
       {networkDisplay}
       <MintButtonRow />
